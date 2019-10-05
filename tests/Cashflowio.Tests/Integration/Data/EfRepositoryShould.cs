1c3de3694d18031e12ca07c1dd4631c5efb8b63f
Jesús Armenta <jesus.armenta1993@gmail.com>
2019-10-05 14:03:35 -0700

    - Actualizar a .NET Core 3.0 - Agregar Syncfusion .NET Core - Hacer consulta básica de prueba

--- tests/Cashflowio.Tests/Integration/Data/EfRepositoryShould.cs
+++ tests/Cashflowio.Tests/Integration/Data/EfRepositoryShould.cs
@@ -19,14 +19,14 @@ namespace Cashflowio.Tests.Integration.Data
             // Create a fresh service provider, and therefore a fresh
             // InMemory database instance.
             var serviceProvider = new ServiceCollection()
-                .AddEntityFrameworkInMemoryDatabase()
+                //.AddEntityFrameworkInMemoryDatabase()
                 .BuildServiceProvider();
 
             // Create a new options instance telling the context to use an
             // InMemory database and the new service provider.
             var builder = new DbContextOptionsBuilder<AppDbContext>();
-            builder.UseInMemoryDatabase("cashflowio")
-                   .UseInternalServiceProvider(serviceProvider);
+            //builder.UseInMemoryDatabase("cashflowio")
+            //       .UseInternalServiceProvider(serviceProvider);
 
             return builder.Options;
         }
