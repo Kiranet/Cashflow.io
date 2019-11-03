9f505fbe42b2ffef624cbaf4673c1f2ff589e145
Jesús Armenta <jesus.armenta1993@gmail.com>
2019-11-03 10:54:11 -0800

    Empezando con pruebas unitarias

---
+++ tests/Cashflowio.Tests/Core/IncomeSourceTests.cs
@@ -0,0 +1,23 @@
+﻿using Cashflowio.Core.Entities;
+using Cashflowio.Core.SharedKernel;
+using Xunit;
+
+namespace Cashflowio.Tests.Core
+{
+    public class IncomeSource : BaseEntity
+    {
+        public string Name { get; set; }
+        public int AvatarId { get; set; }
+        public Avatar Avatar { get; set; }
+    }
+    
+    public class IncomeSourceTests
+    {
+        //TODO: Crear pruebas
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
