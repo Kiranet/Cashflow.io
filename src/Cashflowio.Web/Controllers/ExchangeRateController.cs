9199d850a97492e3ddb7ac5058f710a0bd6aa9c1
Jesús Armenta <jesus.armenta1993@gmail.com>
2019-11-03 08:08:32 -0800

    Separar código que pertenece a Syncfusion fuera del controlador Terminar de implementar el CRUD

--- src/Cashflowio.Web/Controllers/ExchangeRateController.cs
+++ src/Cashflowio.Web/Controllers/ExchangeRateController.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using Cashflowio.Core.Entities;
+﻿using Cashflowio.Core.Entities;
 using Cashflowio.Core.Interfaces;
+using Cashflowio.Web.Libs.Syncfusion;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using Syncfusion.EJ2.Base;
 
 namespace Cashflowio.Web.Controllers
@@ -20,58 +17,35 @@ namespace Cashflowio.Web.Controllers
 
         public IActionResult Index()
         {
-            return View(_repository.List<ExchangeRate>());
-        }
-
-        [HttpPost]
-        public ActionResult Insert([FromBody] CRUDModel model)
-        {
-            var exchangeRate = JsonConvert.DeserializeObject<ExchangeRate>(model.Value.ToString());
-            return Json(_repository.Add(exchangeRate));
-        }
-        
-        public IActionResult Remove([FromBody] CRUDModel model)
-        {
-            var exchangeRate = JsonConvert.DeserializeObject<ExchangeRate>(model.Value.ToString());
-            
-            //TODO: Me quede aqui
-            
-            return Json(_repository.Add(exchangeRate));
+            return View();
         }
 
         public IActionResult DataSource([FromBody] DataManagerRequest dm)
         {
-            IEnumerable DataSource = _repository.List<ExchangeRate>();
-            DataOperations operation = new DataOperations();
-            if (dm.Search != null && dm.Search.Count > 0)
-            {
-                DataSource = operation.PerformSearching(DataSource, dm.Search); //Search
-            }
-
-            if (dm.Sorted != null && dm.Sorted.Count > 0) //Sorting
-            {
-                DataSource = operation.PerformSorting(DataSource, dm.Sorted);
-            }
-
-            if (dm.Where != null && dm.Where.Count > 0) //Filtering
-            {
-                DataSource = operation.PerformFiltering(DataSource, dm.Where, dm.Where[0].Operator);
-            }
-
-            int count = DataSource.Cast<ExchangeRate>().Count();
-            if (dm.Skip != 0)
-            {
-                DataSource = operation.PerformSkip(DataSource, dm.Skip); //Paging
-            }
-
-            if (dm.Take != 0)
-            {
-                DataSource = operation.PerformTake(DataSource, dm.Take);
-            }
-
-            return dm.RequiresCounts ? Json(new {result = DataSource, count = count}) : Json(DataSource);
+            return dm.FilterDataSource(_repository.List<ExchangeRate>());
         }
 
-       
+        [HttpPost]
+        public ActionResult Crud([FromBody] CRUDModel crudModel)
+        {
+            var model = new ExchangeRate();
+
+            switch (crudModel.Action)
+            {
+                case "insert":
+                    model = _repository.Add(crudModel.GetModel<ExchangeRate>());
+                    break;
+                case "update":
+                    model = crudModel.GetModel<ExchangeRate>();
+                    _repository.Update(model);
+                    break;
+                case "remove":
+                    model = _repository.GetById<ExchangeRate>(int.Parse(crudModel.Key.ToString()));
+                    _repository.Delete(model);
+                    break;
+            }
+
+            return Json(model);
+        }
     }
 }
