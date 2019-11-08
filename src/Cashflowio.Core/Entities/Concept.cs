ab25e5e798735bb360587fb7415b8e7791f9a749
Jesús Armenta <jesus.armenta1993@gmail.com>
2019-11-08 14:36:08 -0800

    Migración para las fuentes de ingreso Pruebas unitarias para la lógica

---
+++ src/Cashflowio.Core/Entities/Concept.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cashflowio.Core.Entities
+{
+    public class Concept : Transaction
+    {
+        public DayOfWeek PayDay { get; set; }
+
+        public int DestinationId { get; set; }
+        public MoneyAccount Destination { get; set; }
+
+        public IEnumerable<DateTime> Dates { get; set; }
+    }
+}
