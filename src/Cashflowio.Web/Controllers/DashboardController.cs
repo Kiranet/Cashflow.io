2e09cd00107ba8ffb2a1ccdea0ca1637408338e1
Jesús Armenta <jesus.armenta1993@gmail.com>
2019-12-22 03:53:50 -0800

    Limpiar un poco el proyecto Refactorizar la consulta del dashboard para pasar de la vista a un servicio específico

--- src/Cashflowio.Web/Controllers/DashboardController.cs
+++ src/Cashflowio.Web/Controllers/DashboardController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cashflowio.Web.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cashflowio.Web.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly ITransactionService _transactionService;
+
+        public DashboardController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
         public IActionResult Index(int year)
         {
-            return View(year);
+            return View(_transactionService.Query(year));
         }
     }
 }
---
+++ src/Cashflowio.Web/Models/DashboardViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Cashflowio.Web.Models
+{
+    public class DashboardViewModel
+    {
+        public SelectList Years { get; set; }
+        public int SelectedYear { get; set; }
+        public List<IncomeSourceViewModel> Income { get; set; } = new List<IncomeSourceViewModel>();
+        public List<MoneyAccountViewModel> MoneyAccounts { get; set; } = new List<MoneyAccountViewModel>();
+        public List<ExpenseCategoryViewModel> ExpenseCategories { get; set; } = new List<ExpenseCategoryViewModel>();
+    }
+
+    public class IncomeSourceViewModel
+    {
+        public string Name { get; set; }
+        public double TotalAmount { get; set; }
+        public List<IncomeConceptViewModel> Concepts { get; set; } = new List<IncomeConceptViewModel>();
+    }
+
+    public class IncomeConceptViewModel
+    {
+        public string Description { get; set; }
+        public double Amount { get; set; }
+    }
+
+    public class MoneyAccountViewModel
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public List<MoneyAccountConceptViewModel> Concepts { get; set; } = new List<MoneyAccountConceptViewModel>();
+    }
+
+    public class MoneyAccountConceptViewModel
+    {
+        public string Icon { get; set; }
+        public double Amount { get; set; }
+        public string Name { get; set; }
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ExpenseCategoryViewModel
+    {
+        public string Name { get; set; }
+        public double Amount { get; set; }
+        public List<ExpenseConceptViewModel> Concepts { get; set; } = new List<ExpenseConceptViewModel>();
+    }
+
+    public class ExpenseConceptViewModel
+    {
+        public string Description { get; set; }
+        public double Amount { get; set; }
+    }
+}
